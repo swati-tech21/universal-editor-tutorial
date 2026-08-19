935e94d84ad5bad2b15cbffe2ad5dd61f4d29865
swati-axeno <swati.1@axeno.co>
2026-08-19 12:04:57 +0530

    gjfuy

--- blocks/heading/heading.js
+++ blocks/heading/heading.js
@@ -1,9 +1,17 @@
 export default function decorate(block) {
   const rows = [...block.children];
 
-  const headingText = rows[0]?.textContent.trim();
-  const headingType = rows[1]?.textContent.trim() || 'h2';
-  const alignment = rows[2]?.textContent.trim() || 'left';
+  const headingText = rows[0]?.textContent.trim() || '';
+
+  const possibleHeadingType = rows[1]?.textContent.trim().toLowerCase();
+  const headingType = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(possibleHeadingType)
+    ? possibleHeadingType
+    : 'h2';
+
+  const possibleAlignment = rows[2]?.textContent.trim().toLowerCase();
+  const alignment = ['left', 'center'].includes(possibleAlignment)
+    ? possibleAlignment
+    : 'left';
 
   block.innerHTML = '';
 
